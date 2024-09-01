b34d5352ac6ad0a8480042762c52184e021841be
EfaYukacoonar <efayukacoonar@gmail.com>
2024-09-01 15:36:41 +0900

    Create 01_01-10.c

---
+++ 01_01-10.c
@@ -0,0 +1,17 @@
+/*
+読み込んだ整数の10倍の値を表示
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int no;
+
+    printf("整数を入力してください：");
+    scanf("%d",&no);
+
+    printf("その数の10倍は%dです。\n",10*no);
+
+    retirn(0);
+}
